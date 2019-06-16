c7f299b92083c60c5f85c4f7ffb86cf5922c6371
Matheus Reis <matheusdrdj@gmail.com>
2019-06-16 17:41:43 -0300

    Fixing model for User. Not OR yet

--- api/pulga/models.py
+++ api/pulga/models.py
@@ -22,7 +22,7 @@ class User(models.Model):
     email = models.EmailField(max_length=50)
     nickname = models.CharField(max_length=20)
     addresses = models.ForeignKey(Address, on_delete=models.CASCADE)
-    phones = models.ForeignObject()
+    phones = models.ManyToManyField(Phone)
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
