e00dac1c9b98ff140e5dc6f88163267170ebedbe
Eduardo <eduardo.j.moreira@ufv.br>
2019-06-17 14:11:28 -0300

    Remodelando scripts

--- api/pulga/scripts.sql
+++ api/pulga/scripts.sql
@@ -1,12 +1,54 @@
 BEGIN
-    FOR c IN ( SELECT table_name from user_tables WHERE table_name in ('USERS','ITEMS','SALES','addressesTable','phoneTable')) 
-    LOOP 
-        EXECUTE IMMEDIATE ('DROP TABLE ' || c.table_name || ' CASCADE CONSTRAINTS PURGE'); 
-    END LOOP; 
-END;/
-CREATE OR REPLACE TYPE ADDRESS FORCE AS OBJECT( num INTEGER, street VARCHAR(30), neighborhood VARCHAR(20), city VARCHAR(20))/
-CREATE OR REPLACE TYPE ADDRESSES IS TABLE OF ADDRESS/
-CREATE OR REPLACE TYPE PHONES IS VARRAY(2) OF VARCHAR(15)/
-CREATE TABLE ITEMS( id NUMBER(1,0) PRIMARY KEY, name VARCHAR(30) )/
-CREATE TABLE SALES( id NUMBER(1,0) PRIMARY KEY, time_left NUMBER(2,0), sold_price NUMBER(2), initial_price NUMBER(2), post_date TIMESTAMP(0), done NUMBER(1,0), conclusion DATE)/
-CREATE TABLE USERS ( email VARCHAR(30), fullname VARCHAR(50), nickname VARCHAR(15), addresses ADDRESSES, phones PHONES)NESTED TABLE addresses STORE AS addressesTable
+  FOR c IN ( SELECT object_name, object_type, owner
+             from all_objects
+             WHERE object_name in
+                   ('USERS', 'ITEMS', 'SALES', 'T_USER', 'T_SALE',
+                    'T_ADDRESS', 'T_PHONES', 'T_LIST_ADDRESSES', 'T_ITEM'))
+    LOOP
+      EXECUTE IMMEDIATE ('DROP ' || c.object_type || ' ' || c.owner || '.' || c.object_name || ' FORCE');
+    END LOOP;
+END;
+/
+
+CREATE OR REPLACE TYPE T_ADDRESS FORCE AS OBJECT
+(
+  num          INTEGER,
+  street       VARCHAR(30),
+  neighborhood VARCHAR(20),
+  city         VARCHAR(20)
+)
+/
+CREATE OR REPLACE TYPE T_LIST_ADDRESSES IS TABLE OF T_ADDRESS
+/
+CREATE OR REPLACE TYPE T_PHONES IS VARRAY (2) OF VARCHAR(15)
+/
+CREATE OR REPLACE TYPE T_USER FORCE AS OBJECT
+(
+  email     VARCHAR(30),
+  fullname  VARCHAR(50),
+  nickname  VARCHAR(15),
+  addresses T_LIST_ADDRESSES,
+  phones    T_PHONES
+)
+/
+CREATE OR REPLACE TYPE T_SALE FORCE AS OBJECT
+(
+  time_left     NUMBER(2, 0),
+  sold_price    NUMBER(2),
+  initial_price NUMBER(2),
+  post_date     TIMESTAMP(0),
+  done          NUMBER(1, 0),
+  conclusion    DATE
+)
+/
+CREATE OR REPLACE TYPE T_ITEM FORCE AS OBJECT
+(
+  name VARCHAR(30)
+)
+/
+CREATE TABLE ITEMS of T_ITEM
+/
+CREATE TABLE SALES of T_SALE
+/
+CREATE TABLE USERS of T_USER NESTED TABLE addresses STORE AS addressesTable
+/
