e56bd53c252e2ea4c7f22569b9d62b89de10a259
Matheus Reis <matheusdrdj@gmail.com>
2019-06-19 01:05:18 -0300

    [feat] (Style): Adding style from Semantic-UI + jQuery

---
+++ imports.php
@@ -0,0 +1,3 @@
+<script src="./static/jquery-3.4.1.min.js"></script>
+<script src="./static/semantic.min.js"></script>
+<link rel="stylesheet" type="text/css" href="./static/semantic.min.css">
--- index.php
+++ index.php
@@ -5,21 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pulga</title>
+    <?php require("./imports.php"); ?>
 </head>
 <body>
-<?php 
-    echo "Welcome to our Web Application!";
-
-    try {
-        //                 ($USER,$PSWD,$HOST/$DATABASE)
-        $conn = oci_connect("SYSTEM","Pulga1234","localhost:1521/pulga");
-        if(!$conn){
-            echo "Error when connecting to Database!";
-        }
-    } catch(Exception $e){
-        echo "Error: " + $e;
-    }
-?>
+    <div class="ui container" style="text-align:center; margin-top: 25px">
+        <h1><?php echo "Welcome to our Web Application!"; ?></h1>
+        <?php
+            try {
+                //                 ($USER,$PSWD,$HOST/$DATABASE)
+                $conn = oci_connect("SYSTEM","Pulga1234","localhost:1521/pulga");
+                if(!$conn){
+                    echo "Error when connecting to Database!";
+                }
+            } catch(Exception $e){
+                echo "Error: " + $e;
+            }
+        ?>
+    </div>
 </body>
 </html>
 
