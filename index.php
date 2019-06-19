dbe36c16c64e8b0ae2f9b8ee53a785c33bd44f12
Matheus Reis <matheusdrdj@gmail.com>
2019-06-19 00:53:03 -0300

    [feat] (Database): Database connection stabilished

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pulga</title>
+</head>
+<body>
 <?php 
     echo "Welcome to our Web Application!";
+
+    try {
+        //                 ($USER,$PSWD,$HOST/$DATABASE)
+        $conn = oci_connect("SYSTEM","Pulga1234","localhost:1521/pulga");
+        if(!$conn){
+            echo "Error when connecting to Database!";
+        }
+    } catch(Exception $e){
+        echo "Error: " + $e;
+    }
 ?>
+</body>
+</html>
+
